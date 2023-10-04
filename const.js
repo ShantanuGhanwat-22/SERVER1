bdc6c9408f4db3c36f196de8d6c408af69b8acdb
SHANTANU GHANWAT <ghanwatshantanu2209@gmail.com>
2023-10-04 02:58:54 -0700

    Delete const.js

--- const.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app=express();
-//app.use(express.static('public'));
-app.get('/',(req,res)=>{res.send("welcome");});
-app.listen(9090);
-console.log("App is listening on port 9090");
